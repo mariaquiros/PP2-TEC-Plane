3cb16bac3c58ac7979b527c45eec9db19fcd923c
María <Mara@Maria>
2019-10-29 12:52:11 -0600

    Versión final

---
+++ ProyectoProgramado2/src/main/java/com/proyecto2/ProyectoProgramado2/App.java
@@ -0,0 +1,13 @@
+package com.proyecto2.ProyectoProgramado2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
